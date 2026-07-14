ce6da7d7ed39b25746adbd027ef51dd82c61a678
Duckxmeta <kpkinkin@gmail.com>
2026-07-14 00:02:17 +0000

    Deploying to gh-pages from @ Duckxmeta/JustDuckit-app@4587c7d3b095c575c061315295152fea6c5ad0a3 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2771179190" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3373487013" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
